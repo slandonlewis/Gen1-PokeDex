a56f5bcf24b57e04dee09d088842bd53e7660319
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2023-01-15 17:24:46 -0600

    created functionality for adding pokemon to personal database

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const searchBar = document.querySelector('#search')
 const button = document.querySelector('#btn')
+const addBtn = document.querySelector('#add')
 const infoSection = document.querySelector('#info')
+const ownEl = document.querySelector('#own')
 const api = 'https://pokeapi.co/api/v2/pokemon'
+let currentPokemon
+let caught = []
 
 button.addEventListener('click', (evt) => {
     let inputValue = searchBar.value
@@ -12,6 +16,16 @@ button.addEventListener('click', (evt) => {
     }
 })
 
+addBtn.addEventListener('click', (evt) => {
+    if (currentPokemon.owned === true) {
+        window.alert('Pokémon has already been caught')
+    } else {
+        currentPokemon.owned = true
+        caught.push(currentPokemon)
+        ownEl.innerHTML = `Total Caught: ${caught.length}`
+    }
+})
+
 const searchPokémon = (input) => {
     fetch(`${api}/${input.toLowerCase()}`)
         .then(res => res.json())
@@ -19,11 +33,13 @@ const searchPokémon = (input) => {
             (pokemonData) => {
                 infoSection.innerHTML =
                     `<img src=${pokemonData.sprites.front_default}><br>
-                    Name: ${pokemonData.name.toUpperCase()}<br>
-                    Type: ${pokemonData.types.map(_type => ` ${_type.type.name.toUpperCase()}`)}<br>
-                    Weight: ${pokemonData.weight}<br>
-                    Height: ${pokemonData.height}<br>
-                    Moves: <br>${pokemonData.moves.map(_moves => ` ${_moves.move.name}`)}<br>`
+            Name: ${pokemonData.name.toUpperCase()}<br>
+            Type: ${pokemonData.types.map(_type => ` ${_type.type.name.toUpperCase()}`)}<br>
+            Weight: ${pokemonData.weight}<br>
+            Height: ${pokemonData.height}<br>
+            Moves: <br>${pokemonData.moves.map(_moves => ` ${_moves.move.name}`)}<br>`
+                addBtn.className = ''
+                currentPokemon = pokemonData
             }
         )
 }
