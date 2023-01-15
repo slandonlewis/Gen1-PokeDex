f5b3c574c892d88837ced77adde3279c6bb8512f
Seth Lewis <97172419+slandonlewis@users.noreply.github.com>
2023-01-14 19:36:18 -0600

    bare minimum layout

---
+++ index.js
@@ -0,0 +1,7 @@
+const searchBar = document.querySelector('#search')
+const button = document.querySelector('#btn')
+const infoSection = document.querySelector('#info')
+
+const searchPokémon = () => {
+    infoSection.innerHTML = 'test'
+}
